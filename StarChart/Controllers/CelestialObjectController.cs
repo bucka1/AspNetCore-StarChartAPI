9cb529b804c5d780cf18ba8be962bcc985a7020c
Austin Buck <bucka1@michigan.gov>
2022-03-14 08:27:54 -0400

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,5 +14,44 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById (int Id)
+        {
+            var Satellites = _context.CelestialObjects.FirstOrDefault(i => i.OrbitedObjectId == Id);
+
+            if (Satellites != null )
+            {
+                return Ok(Satellites);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var Satellites = _context.CelestialObjects.FirstOrDefault(n => n.Name == name);
+
+            if(Satellites != null)
+            {
+                return Ok(name);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var Satellites = _context.CelestialObjects.ToList();
+
+            return Ok(Satellites);
+        }
+
     }
 }
